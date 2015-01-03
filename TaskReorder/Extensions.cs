963b637897ed5254e774e70c83b51596b9fbc03e
ZanyGnu <ajaymartin@gmail.com>
2015-01-02 20:30:21 -0800

    basic implementation to allow modifying a one note task list

---
+++ TaskReorder/Extensions.cs
@@ -0,0 +1,33 @@
+﻿
+namespace TaskReorder
+{
+    using Microsoft.Office.Interop.OneNote;
+    using System.Linq;
+    using System.Xml.Linq;
+
+    public static class ApplicationExtensions
+    {
+        public static XDocument GetPageContents(this Application onenoteApp, string notebookName, string sectionName, string pageName)
+        {
+            string notebookXml;
+
+            onenoteApp.GetHierarchy(null, HierarchyScope.hsPages, out notebookXml);
+
+            var doc = XDocument.Parse(notebookXml);
+            var ns = doc.Root.Name.Namespace;
+            var pageNode = doc.Descendants(ns + "Page").Where(
+                n => n.Attribute("name").Value == pageName
+                && n.Parent.Attribute("name").Value == sectionName
+                && n.Parent.Parent.Attribute("name").Value == notebookName).FirstOrDefault();
+
+            if (pageNode != null)
+            {
+                string pageXml;
+                onenoteApp.GetPageContent(pageNode.Attribute("ID").Value, out pageXml);
+                return XDocument.Parse(pageXml);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ TaskReorder/Program.cs
@@ -0,0 +1,57 @@
+﻿
+namespace TaskReorder
+{
+    using Microsoft.Office.Interop.OneNote;
+    using System;
+    using System.Collections.Generic;
+    using System.Xml.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application onenoteApp = new Application();
+            
+            XDocument page = onenoteApp.GetPageContents("TestOneNote", "TestSection", "Shopping List");
+
+            XNamespace ns = page.Root.Name.Namespace;
+
+            XElement oeElementsParent = page
+                .Element(ns + "Page")
+                .Element(ns + "Outline")
+                .Element(ns + "OEChildren");
+
+            List<XElement> completedElements = new List<XElement>();
+            List<XElement> newCuratedElementList = new List<XElement>();
+
+            foreach (XElement oeElement in oeElementsParent.Elements(ns + "OE"))
+            {
+                XElement tag = oeElement.Element(ns + "Tag");
+                if (tag != null)
+                {
+                    if (tag.Attribute("completed").Value == "true")
+                    {
+                        completedElements.Add(oeElement);
+                        continue;
+                    }
+                }
+
+                newCuratedElementList.Add(oeElement);
+            }
+
+            foreach (XElement completedElement in completedElements)
+            {
+                newCuratedElementList.Add(completedElement);
+            }
+
+            oeElementsParent.RemoveAll();
+
+            foreach (XElement curatedElement in newCuratedElementList)
+            {
+                oeElementsParent.Add(curatedElement);
+            }
+
+            onenoteApp.UpdatePageContent(page.ToString(), DateTime.MinValue);
+        }
+    }
+}
