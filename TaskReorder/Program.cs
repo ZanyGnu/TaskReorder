df9130b8dbcd632a3cbae594bea394f7ef1a18e8
ZanyGnu <ajaymartin@gmail.com>
2015-03-01 08:44:06 -0800

    run task reorder in the background and use AppExtensions

--- TaskReorder/Program.cs
+++ TaskReorder/Program.cs
@@ -1,18 +1,60 @@
 ﻿
 namespace TaskReorder
 {
+    using AppExtensions;
     using Microsoft.Office.Interop.OneNote;
     using System;
     using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
     using System.Xml.Linq;
 
     class Program
     {
+        private static string UniqueApplicationId = "4C2DE1C4-DF3E-439D-8ECF-0B258A679A59";
+
         static void Main(string[] args)
+        {
+            CurrentApplication.EnsureSingleInstance(UniqueApplicationId);
+            CurrentApplication.MakeProgramAutoRun();
+            CurrentApplication.EnsureBackgroundWorker();
+
+            // Have this app running always periodically waking up
+            BlindlyRun(ReorderTasks, TimeSpan.FromSeconds(10));
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        public static async Task BlindlyRun(Action action, TimeSpan period, CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(period, cancellationToken);
+                
+                try
+                {
+                    action();
+                }
+                catch
+                {
+                    // method is blind because it doesnt care about exceptions. sigh.
+                }
+            }
+        }
+
+        public static Task BlindlyRun(Action action, TimeSpan period)
+        {
+            return BlindlyRun(action, period, CancellationToken.None);
+        }
+                
+        private static void ReorderTasks()
         {
             Application onenoteApp = new Application();
             
-            XDocument page = onenoteApp.GetPageContents("TestOneNote", "TestSection", "Shopping List");
+            XDocument page = onenoteApp.GetPageContents(
+                "Ajay-Preethi Shared OneNotes", 
+                "Our Notes", 
+                "Shopping List");
 
             XNamespace ns = page.Root.Name.Namespace;
 
